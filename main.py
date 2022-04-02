244fa605013a01de1e688395640f39097269bbab
Ritik Ranjan <ritik0ranjan@gmail.com>
2022-04-02 13:56:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -67,4 +67,4 @@ async def cricket_api(url: Optional[str] = None) -> Optional[Dict[str, Any]]:
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
+    uvicorn.run(app, port=1729, debug=True)
